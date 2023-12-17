31f26d4166dc98df6de07fe0515a0f7fd8e9c8fc
PREM KUMAR PATHAK <96000681+nsprem114@users.noreply.github.com>
2023-12-17 21:10:19 +0530

    first commit

---
+++ src/Data/templates.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import templateOneImg from "../Images/resume-one.png";
+import templateTwoImg from "../Images/resume-two.png";
+import Template1 from "../Templates/Template1";
+import Template2 from "../Templates/Template2";
+
+export const templates = [
+  {
+    id: 1,
+    template_name: "Template One",
+    template_img: templateOneImg,
+    template: <Template1 />,
+  },
+  {
+    id: 2,
+    template_name: "Template Two",
+    template_img: templateTwoImg,
+    template: <Template2 />,
+  },
+];
